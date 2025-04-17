1a3c04ff4fe8473d834c605d29a2a9c4ba11e957
Nicolas Trindade <nico.trindade2@gmail.com>
2025-04-17 11:56:20 -0300

    Projeto sem os códigos

---
+++ Principal/src/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Teste");
+    }
+    
+}
