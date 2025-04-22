a36863f621fd16d87f11e34bfe754c143d9e43f6
Nicolas Trindade <nico.trindade2@gmail.com>
2025-04-22 08:42:21 -0300

    Update EntradaSaidaDados.java
    joptionpane

--- Principal/src/principal/EntradaSaidaDados.java
+++ Principal/src/principal/EntradaSaidaDados.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package principal;
+package principalgui;
 
-/**
- *
- * @author Admin
- */
-public class EntradaSaidaDados {
-    
+import javax.swing.JOptionPane; 
+
+/** 
+ * Classe para lidar com a entrada e saída de dados usando interfaces gráficas simples (JOptionPane).
+ */ 
+public class EntradaSaidaDados { 
+
+    // Construtor vazio.
+    public EntradaSaidaDados() { 
+    } 
+
+    public String entradaDados(String mensagemEntrada) { 
+        String input = JOptionPane.showInputDialog(null, mensagemEntrada, "Entrada de Dados", JOptionPane.QUESTION_MESSAGE);
+        return input; 
+    } 
+
+    /** 
+     * Mostra uma caixa de diálogo simples exibindo uma mensagem para o usuário.
+     * @param mensagemSaida A mensagem a ser exibida na caixa de diálogo. 
+     */ 
+    public void saidaDados(String mensagemSaida) { 
+        JOptionPane.showMessageDialog(null, mensagemSaida, "Informação", JOptionPane.INFORMATION_MESSAGE);
+    } 
+
+    public void saidaDadosErro(String mensagemErro) { 
+        JOptionPane.showMessageDialog(null, mensagemErro, "Erro", JOptionPane.ERROR_MESSAGE);
+    } 
 }
